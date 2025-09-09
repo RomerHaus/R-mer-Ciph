ace11e3cf120a73ed92b4eb40c805d434e00c35c
RomerHaus <RomerFurniture@gmail.com>
2025-09-08 20:21:18 -0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,8 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import {createRoot} from 'react-dom/client';
+import App from './App';createRoot(document.getElementById('root')!).render(<App/>);
+
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
