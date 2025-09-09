d44e9f7938b01e314c8b6fc5abe1c45685dedfc6
RomerHaus <RomerFurniture@gmail.com>
2025-09-08 21:58:09 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -51,7 +51,7 @@ const t = setTimeout(() => setGenerationResult(null), 5000);
 return () => clearTimeout(t);
 }, \[generationResult]);
 
-return ( <div className="bg-gray-950 min-h-screen text-gray-300"> <Header /> <main className="container mx-auto px-4 py-8"> <div className="grid grid-cols-1 lg:grid-cols-5 gap-8"> <div className="lg:col-span-3"><SkuForm onGenerate={handleGenerate} /></div> <div className="lg:col-span-2"><ResultsDisplay result={generationResult} /></div> </div>
+return ( <div className="bg-gray-950 min-h-screen text-gray-300"> <Header /> <main className="container mx-auto px-4 py-8"> <div className="grid grid-cols-1 lg:grid-cols-5 gap-8"> <div className="lg:col-span-3"><SkuForm onGenerate={handleGenerate} formData={formData} setFormData={setFormData} /></div> <div className="lg:col-span-2"><ResultsDisplay result={generationResult} /></div> </div>
 {generatedHistory.length > 0 && ( <HistoryDisplay
          history={generatedHistory}
          buildName={buildName}
